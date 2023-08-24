ce74249a255c5dfa397b3bfd39f1869119830cb2
Kanan Niftiyev <47481598+kananniftiyev@users.noreply.github.com>
2023-08-24 13:00:42 +0400

    Setup

---
+++ src/main/java/dev/kananniftiyev/GameReviewHub/GameReviewHubApplication.java
@@ -0,0 +1,13 @@
+package dev.kananniftiyev.GameReviewHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameReviewHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameReviewHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/kananniftiyev/GameReviewHub/GameReviewHubApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kananniftiyev.GameReviewHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameReviewHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
