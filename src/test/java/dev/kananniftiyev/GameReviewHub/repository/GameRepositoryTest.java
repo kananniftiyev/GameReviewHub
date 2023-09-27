b745ad344918c4fefc70a5b3f88f812fdb23fdcd
Kanan Niftiyev <47481598+kananniftiyev@users.noreply.github.com>
2023-09-27 15:35:45 +0200

    Fix bug

--- src/test/java/dev/kananniftiyev/GameReviewHub/repository/GameRepositoryTest.java
+++ src/test/java/dev/kananniftiyev/GameReviewHub/repository/GameRepositoryTest.java
@@ -9,17 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.TestPropertySource;
 
 import dev.kananniftiyev.GameReviewHub.entity.Game;
 
 @DataJpaTest
-@TestPropertySource(properties = {
-        "spring.datasource.url=jdbc:postgresql://localhost:5432/GameReviewHub",
-        "spring.datasource.username=postgres",
-        "spring.datasource.password=kanan123",
-        "spring.datasource.driver-class-name=org.postgresql.Driver"
-})
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class GameRepositoryTest {
 
     @Autowired
